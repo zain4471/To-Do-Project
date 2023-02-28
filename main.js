e210a15d54a24e707ccbd619c0edc5fa7bed8c58
zain <zainy9505@gmail.com>
2023-02-28 14:58:17 +0500

    Changed the edit Method and Add Edit Button

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 // create the cross icon
 let iconList = document.getElementsByTagName("li");
-
 for (let i = 0; i < iconList.length; i++) {
   let spanTag = document.createElement("span");
   let iconInSpan = document.createTextNode("\u00D7");
   spanTag.className = "cross";
   spanTag.appendChild(iconInSpan);
   iconList[i].appendChild(spanTag);
-  iconList[i].onclick = function () {
-    iconList[i].contentEditable = true;
+
+    //CREATE EDIT BUTTON
+
+  let editSpan = document.createElement("button");
+  let editIcon = document.createTextNode("Edit");
+  editSpan.className = "edit-btn";
+  editSpan.appendChild(editIcon)
+  iconList[i].appendChild(editSpan)
+
+  editSpan.onclick = function () {
+    editEelement(iconList[i])
   }
 }
 
@@ -19,7 +27,6 @@ for (let j = 0; j < crossBtn.length; j++) {
   crossBtn[j].onclick = function () {
     let removeItem = this.parentElement;
     removeItem.remove();
-    // console.log(removeItem);
   };
 }
 
@@ -31,15 +38,22 @@ let addNewList = () => {
   let creatNode = document.createTextNode(inputData);
   creatLi.appendChild(creatNode);
   mainList.appendChild(creatLi);
-  
-  // UPDATE FUNCTION
-  mainList.onclick = function () {
-    creatLi.contentEditable = true;
-  };
+
+
 
   // Empty The Input Vlaue After Adding a Value in List
 
   document.getElementById("input-data").value = "";
+   //Adding edit Button
+   let editSpan = document.createElement("button");
+   let editIcon = document.createTextNode('Edit');
+   editSpan.className = "edit-btn";
+   editSpan.appendChild(editIcon)
+   creatLi.appendChild(editSpan);
+
+   editSpan.onclick = function () {
+    editEelement(creatLi)
+  };
 
   // Adding cross Icon
 
@@ -55,7 +69,13 @@ let addNewList = () => {
     crossBtn[k].onclick = function () {
       let removeItem = this.parentElement;
       removeItem.remove();
-      // console.log(removeItem);
     };
   }
 };
+
+//Edit METHOD
+
+function editEelement(e) {
+  let editValue = prompt('edit the select item', e.firstChild.nodeValue);
+  e.firstChild.nodeValue = editValue;
+}
